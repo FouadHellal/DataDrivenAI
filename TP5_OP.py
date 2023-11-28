5c62dc618e32ea086a751271080af234d14c9a2c
FouadHellal <113594352+FouadHellal@users.noreply.github.com>
2023-11-28 17:35:31 +0100

    Initial commit

---
+++ TP5_OP.py
@@ -0,0 +1,148 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler,LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,accuracy_score, classification_report, ConfusionMatrixDisplay
+import os
+import tensorflow as tf # importer tensorflow
+from tensorflow import keras # importer Keras
+from keras.models import Sequential # Créer le modèle multicouche
+from keras import optimizers # importer les optimiseurs : SGD, Adam, RMSprop
+from keras.utils import to_categorical # transformer les labels y en vecteurs canoniques
+from tensorflow.keras.layers import Conv1D, LeakyReLU, MaxPooling1D, Flatten, Dense, BatchNormalization, Dropout, GlobalAveragePooling1D
+
+LTE_data = pd.read_csv("C:/Users/helfo/Downloads/LTE_new_data.csv", index_col=None)
+Base=LTE_data[['RSRP', 'RSRQ','RSSI', 'SNR','DL_bitrate', 'UL_bitrate', 'path' ]]
+Base=Base.replace("-", 0)
+Appr=Base.drop('path', axis=1)
+classe2=Base['path'] #Extrait la colonne path (variable cible)
+scaler2 = StandardScaler()
+A=scaler2.fit_transform(Appr) #Standardisation 
+le= LabelEncoder() #convertit les str en valeurs numériques
+classe = le.fit_transform(classe2)
+
+App, test, y_train, y_test = train_test_split(A, classe, test_size=0.2,
+                                                            random_state=42)
+L_App = to_categorical(y_train)
+L_test = to_categorical(y_test)
+
+'''-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._'''
+#3 Simple CNN layers with Flatten then dense
+def cnn(kpi, classes):
+    model = Sequential()
+    
+    model.add(Conv1D(32, kernel_size=3, input_shape=(kpi, 1), padding='same'))
+    #padding='same' ensures that the spatial dimensions of the input remain the same after the convolution operation, preventing information loss at the borders of the input
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling1D())
+    
+    model.add(Conv1D(64, kernel_size=3, padding='same'))
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling1D())
+    
+    model.add(Conv1D(128, kernel_size=3, padding='same'))
+    model.add(LeakyReLU(alpha=0.01))
+    
+    model.add(Flatten())
+    
+    model.add(Dense(128))
+    model.add(Dense(60))    
+    model.add(Dense(classes, activation='softmax'))
+    
+    return model
+
+#3 Simple CNN layers then BatchNormalization with Flatten then dense
+def cnn_batch_norm(kpi, classes):
+    model = Sequential()
+    
+    model.add(Conv1D(32, kernel_size=3, input_shape=(kpi, 1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling1D())
+
+    
+    model.add(Conv1D(64, kernel_size=3, padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling1D())
+    
+    model.add(Conv1D(128, kernel_size=3, padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    
+    model.add(Flatten())
+    model.add(Dropout(rate=0.15))
+  
+    model.add(Dense(128))    
+    model.add(Dense(60))    
+    model.add(Dense(classes, activation='softmax'))
+    
+    return model
+
+#3 Simple CNN layers then BatchNormalization with GAP instead of flatten-dense
+def cnn_GAP_fct(kpi, classes, fct):
+    model = Sequential()
+    
+    model.add(Conv1D(32, kernel_size=3, input_shape=(kpi, 1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling1D())
+
+    model.add(Conv1D(64, kernel_size=3, padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling1D())
+
+    model.add(Conv1D(128, kernel_size=3, padding='same'))
+    model.add(BatchNormalization())
+    
+    # Global Average Pooling
+    model.add(GlobalAveragePooling1D())  
+
+    model.add(Dense(classes, activation=fct))
+    
+    return model
+
+'''-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._'''
+#TRY YOUR MODEL HERE :
+def evaluate_model(kpi,classes,fct,batch,i):
+    
+    if i==1:
+        model = cnn(kpi, classes)
+    elif i==2:
+        model = cnn_batch_norm(kpi, classes)
+    elif i==3:
+        model = cnn_GAP_fct(kpi, classes,fct)
+    
+    #architecture du modèle
+    model.summary()
+    
+    # COMPILER
+    # On spécifie comment le modèle doit apprendre
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=0.008),
+                  metrics=['accuracy'])
+    
+    history = model.fit(
+        App,                     # Données d'Apprentissage
+        L_App,                   # Étiquettes d'Apprentissage
+        epochs=500,              # Nombre d'époques d'entraînement
+        batch_size=batch,           # Taille du lot (nombre d'échantillons par mise à jour des poids)
+        steps_per_epoch=len(App) / batch,  # Nombre d'étapes par époque (nombre total d'échantillons divisé par la taille du lot)
+    )
+    
+    # Évaluation finale sur les données de test
+    _, final_accuracy = model.evaluate(test, L_test)
+    print("Accuracy des données de test:", final_accuracy*100,'%')
+    
+    pred=model.predict(test)
+    y_pred = np.argmax(pred, axis=1)
+    
+    # de confusion
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=le.classes_)
+    disp.plot()
+
+ii=input('choisissez votre cnn :')
+evaluate_model(kpi=6,classes=5, fct='relu', batch=32,i=int(ii))
---
+++ TP5_POTATO.py
@@ -0,0 +1,159 @@
+'''-----------------------TP WITH IMAGES POTATO ----------------------------------------------'''
+import numpy as np
+import pandas as pd
+import os
+import cv2
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+import warnings
+import tensorflow as tf # importer tensorflow
+from tensorflow import keras # importer Keras
+from keras.models import Sequential # Créer le modèle multicouche
+from keras import optimizers # importer les optimiseurs : SGD, Adam, RMSprop
+from keras.utils import to_categorical # transformer les labels y en vecteurs canoniques
+from tensorflow.keras.layers import Conv2D,GlobalAveragePooling2D,AveragePooling2D,MaxPooling2D,Conv1D, LeakyReLU, MaxPooling1D, Flatten, Dense, BatchNormalization, Dropout, BatchNormalization, GlobalAveragePooling1D
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+
+Liste = [
+    "C:/Users/helfo/Downloads/Dataset/Potato_Late_blight",
+    "C:/Users/helfo/Downloads/Dataset/Potato_Early_blight",
+    "C:/Users/helfo/Downloads/Dataset/Potato_healthy"
+    ]
+
+# Fonction pour générer les caractéristiques
+def preTrait(folder, data):
+    for pic in os.listdir(folder):
+        img = plt.imread(os.path.join(folder, pic))
+        ima=cv2.resize(img, (40,40), interpolation = cv2.INTER_AREA)
+        data.append(ima)
+    return data
+
+# Création de la base de données
+Base = []
+for i in range(len(Liste)):
+    preTrait(Liste[i], Base)  
+
+Base = np.array(Base)
+
+# Création des étiquettes
+labels = []
+for i in range(len(Liste)):
+    classe_dir = Liste[i]
+    image_files = [file for file in os.listdir(classe_dir) if '.' in file]
+    class_index_labels = [i] * len(image_files)
+    labels.extend(class_index_labels)
+
+# Convertir la liste en un tableau NumPy
+labels = np.array(labels)
+#split
+App, test, y_train, y_test = train_test_split(Base, labels,
+                                              test_size=0.2,
+                                              random_state=42)
+#canonisation
+L_App = to_categorical(y_train)
+L_test = to_categorical(y_test)
+
+'''-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._-._.-._'''
+
+def modele2(input_shape, num_classes,n_filter):
+    model = Sequential()
+    
+    model.add(Conv2D(32, kernel_size=(n_filter, n_filter), input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(64, kernel_size=(n_filter, n_filter)))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(128, kernel_size=(n_filter, n_filter)))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+
+    model.add(Flatten())
+
+    model.add(Dense(128))
+    model.add(Dense(60))
+    model.add(Dense(num_classes, activation='softmax'))
+    
+    return model
+
+def modele2_GAP(input_shape, num_classes,n_filter):
+    model = Sequential()
+    
+    model.add(Conv2D(32, kernel_size=(n_filter, n_filter), input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(64, kernel_size=(n_filter, n_filter)))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(128, kernel_size=(n_filter, n_filter)))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.01))
+    
+    model.add(GlobalAveragePooling2D())  
+
+    model.add(Dense(num_classes, activation='relu'))
+    
+    return model
+
+input_shape = (40, 40, 3)  # Modifier la taille en fonction de vos images
+num_classes = 3  # Nombre de classes dans votre ensemble de données
+model = modele2_GAP(input_shape, num_classes,5)
+#model = modele2_GAP(input_shape, num_classes,5)
+
+#architecture du modèle
+model.summary()
+
+#COMPILER
+# On spécifie comment le modèle doit apprendre
+model.compile(loss='categorical_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(learning_rate=0.008),
+              metrics=['accuracy'])
+
+
+history = model.fit(
+    App,                     # Données d'Apprentissage
+    L_App,                   # Étiquettes d'Apprentissage
+    epochs=200,              # Nombre d'époques d'entraînement
+    batch_size=16,           # Taille du lot (nombre d'échantillons par mise à jour des poids)
+    steps_per_epoch=len(App) /16,  # Nombre d'étapes par époque (nombre total d'échantillons divisé par la taille du lot)
+)
+# Évaluation finale sur les données de test
+_, final_accuracy = model.evaluate(test, L_test)
+print("Accuracy des données de test:", final_accuracy*100 ,'%')
+
+pred=model.predict(test)
+
+# Convertir les prédictions en classes
+y_pred = np.argmax(pred, axis=1)
+
+# Calculer la matrice de confusion
+cm = confusion_matrix(y_test, y_pred)
+le = LabelEncoder()
+le.fit(labels)
+
+# Afficher la matrice de confusion
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=le.classes_)
+disp.plot()
+
+# Afficher le rapport de classification
+report = classification_report(y_test, y_pred, target_names=None)
+print(report)
+
+#Accuracy des données de test: 92.79279112815857 % % Batch = 32 
+#Accuracy des données de test: 90.09009003639221 % Batch = 16 
+
+#Accuracy des données de test: 86.48648858070374 % 5*5 filters
+
+#Accuracy des données de test: 94.59459185600281 % using GAP / 5*5 filters
